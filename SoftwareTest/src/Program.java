ec8494c8d1d8c73340d735090efb29a4cb7a4439
reecejamespeters <reecepeters2404@gmail.com>
2020-02-26 22:42:44 +0200

    Did some stuff

--- SoftwareTest/src/Program.java
+++ SoftwareTest/src/Program.java
@@ -2,9 +2,10 @@
 public class Program {
 	public static void main(String [] args) {
 		System.out.println("Dino Anastasopoulos");
-		System.out.println("Goolam");
+		System.out.println("Goolam Bangie");
 		System.out.println("Jesse Bristow");
 		System.out.println("Philani Mpofu");
 		System.out.println("Chloë Smith");
+		System.out.println("Reece Peters");
 	}
 }
