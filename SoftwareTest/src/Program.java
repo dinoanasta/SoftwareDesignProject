2ede82e23b507def86cc61ac9522973e26d41705
JesseBristow <1875955@students.wits.ac.za>
2020-02-26 10:51:10 +0200

    Added my name

--- SoftwareTest/src/Program.java
+++ SoftwareTest/src/Program.java
@@ -3,5 +3,6 @@ public class Program {
 	public static void main(String [] args) {
 		System.out.println("Dino Anastasopoulos");
 		System.out.println("Goolam");
+		System.out.println("Jesse Bristow");
 	}
 }
