ad39134759c51dbfa3520f44b1867ed16c795505
JesseBrostow <61417148+JesseBrostow@users.noreply.github.com>
2020-10-13 13:42:37 +0200

    Update Marker.js

--- GraphGenerator/js/Marker.js
+++ GraphGenerator/js/Marker.js
@@ -22,10 +22,37 @@ class Marker{
         return flag;
     }
 
-    markShortestPath(memoGraph,answerGraph,isMemoQuestionGraph){
+    markShortestPath(memoGraph,answerGraph,isMemoQuestionGraph,isBFS){
+        if(isBFS){
+            // convert all edge weights to 1
+            var memoGraphAdjMat = memoGraph.getAdjacenyMatrix();
+            var answerGraphAdjMat = answerGraph.getAdjacenyMatrix();
+            var curr_src_node = memoGraph.getSourceNode();
+
+            memoGraphAdjMat = this.convertAdjMatWeights(memoGraphAdjMat);
+            answerGraphAdjMat = this.convertAdjMatWeights(answerGraphAdjMat);
+
+            memoGraph = new Graph(memoGraphAdjMat);
+            answerGraph = new Graph(answerGraphAdjMat);
+
+            memoGraph.setSourceNode(curr_src_node);
+            answerGraph.setSourceNode(curr_src_node);
+        }
+
         let m = new ShortestPathMarker(memoGraph,answerGraph,isMemoQuestionGraph);
         var flag = m.checkAnswerShortestPath();
         return flag;
+    }
+
+    convertAdjMatWeights(adj_mat){
+        for(var i=0;i<adj_mat.length;i++){
+              for(var j=0;j<adj_mat.length;j++){
+                  if(adj_mat[i][j]!=0){
+                      adj_mat[i][j] = 1;
+                  }
+              }
+        }
+        return adj_mat;
     }
 
 }
